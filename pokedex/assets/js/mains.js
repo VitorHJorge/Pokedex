f20535cd77282ae3229f4945dd3092984c318981
VitorHJorge <vitorhenrique091@outlook.com>
2023-04-23 18:08:38 -0300

    Começando as requisições da API

--- pokedex/assets/js/mains.js
+++ pokedex/assets/js/mains.js
@@ -1,10 +1,33 @@
 
+function convertPokemonToLi (pokemon) {
+    return `
+        <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">Grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="">
+        </div>
+    </li>
+    `
+}
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function(response){
-    console.log(response)
-})
+const pokemonList = document.getElementById("pokemonList")
 
+pokeApi.getPokemons().then((pokemons = []) => {
+
+    const newList = pokemons.map((pokemon) => {
+        return convertPokemonToLi(pokemon)
+
+    })
+
+    const newHTML = newList.join()
+
+    pokemonList.innerHTML == newHTML
+
+    })
+    .catch((error) => {console.error(error)})
